f5868c80fe591600fa41381eae77f50749070e2d
Earth3300 <cbos@tnoep.ca>
2018-11-21 10:21:16 -0500

    Update css.php

--- css/css.php
+++ css/css.php
@@ -26,6 +26,11 @@
  * Text Domain: ec01-css
  * License:  GPL v3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * File: css.php
+ * Created: 2018
+ * Updated: 2018-11-03
+ * Time: 10:49 EST
  */
 
 namespace Earth3300\EC01;
@@ -37,34 +42,49 @@ namespace Earth3300\EC01;
  * and the switch for determining the context in which this file
  * is found.
  */
-class CSS_Processor
+class CSSProcessor
 {
 
 	/** @var array Default options. */
 	protected $opts = [
 		'title' => 'EC01 CSS Processor',
-		'max' => [ 'files' => 8, 'length' => 75000 ],
+		'max' => [
+							'files' => 15,
+							'length' => 99000,
+							],
+		'med' => [
+							'files' => 5,
+							'length' => 50000,
+							],
+		'min' => [
+							'files' => 1,
+							'length' => 30000,
+							],
 		'mime' => [
-				   'type' => 'text',
-				   'subtype' => 'plain',
-				   'ext' => '.css'
-				   ],
-		'exclude' => [ 'style.all.css', 'style.min.css' ],
-		'file' => [ 'write' =>
-					[
-					'all' => 'style.all.css',
-					'min' => 'style.min.css' ]
-				   ],
-		'message' =>
-					[
+						   'type' => 'text',
+						   'subtype' => 'plain',
+						   'ext' => '.css',
+				   		],
+		'exclude' => [
+								'style.css',
+								'style.all.css',
+								'style.min.css',
+							],
+		'file' => [
+							'write' => [
+							'all' => 'style.all.css',
+							'med' => 'style.css',
+							'min' => 'style.min.css' ],
+					   	],
+		'message' => [
 					  'na' => 'Not available.',
-					  'write' =>
-						[
+					  'write' => [
 						'denied' => 'Write permission denied.' ,
 						'success' => 'Write operation succeeded.',
 						'failure' => 'Write operation failed.',
 						],
-					]
+					],
+		'link' => 'https://github.com/earth3300/ec01-css',
 			];
 
 	/**
@@ -110,7 +130,6 @@ class CSS_Processor
 					}
 					else
 					{
-
 						$msg .= sprintf( '<p>%s</p>%s', $this->opts['message']['write']['failure'], PHP_EOL );;
 					}
 				}
@@ -145,6 +164,9 @@ class CSS_Processor
 		$files['str'] = '';
 		$files['files'] = [];
 
+		/** Get the maximum number of files to process. */
+		$max_files = $this->getMaxFiles();
+
 		/** Set the count to zero and stop processing if more than allowed. */
 		$cnt = 0;
 
@@ -165,10 +187,12 @@ class CSS_Processor
 			if (
 				! empty( $file )
 				&& is_string( $file )
-				&& ! in_array( $file, $this->opts['exclude'] ) )
+				&& ! in_array( $file, $this->opts['exclude'] )
+				&& strpos( $file, '-dnp.') === FALSE
+				)
 				{
 					/** Make sure we haven't gone over the number of files allowed */
-					if ( $cnt < $this->opts['max']['files'] )
+					if ( $cnt < $max_files )
 					{
 						/** Get the contents of the file. */
 						$contents = file_get_contents( $file_and_path );
@@ -201,6 +225,27 @@ class CSS_Processor
 	}
 
 	/**
+   * Get the maximum number of files to process.
+	 *
+	 * Checks to see if the med parameter is set. If it is, return that, else
+	 * use the max allowed set in opts.
+	 *
+	 * @return int
+	 */
+	 private function getMaxFiles()
+	 {
+			 if( isset( $_GET['med']))
+	 		{
+	 			$max_files = $this->opts['med']['files'];
+	 		}
+	 		else {
+	 			$max_files = $this->opts['max']['files'];
+	 		}
+			return $max_files;
+	 }
+
+
+	/**
 	 * Get the File Name from the File and Path String.
 	 *
 	 * @param string $file_and_path
@@ -226,6 +271,24 @@ class CSS_Processor
 		}
 	}
 
+  /**
+   * Minify
+   *
+   */
+  private function minify( $str = '' )
+  {
+    /**  Ensure the string length is non-trivial. */
+    if (  0 && strlen( $str ) > 1000 )
+    {
+      $minify = new Minifier();
+      $minify->css( $str );
+    }
+    else
+    {
+      return false;
+    }
+  }
+
 	/**
 	 * Write the files to storage.
 	 *
@@ -237,7 +300,17 @@ class CSS_Processor
 	{
 		if ( ! empty( $str ) )
 		{
-			$file = $this->opts['file']['write']['all'];
+			if ( isset( $_GET['med'] ) )
+			{
+				$file = $this->opts['file']['write']['med'];
+			}
+			elseif ( isset ( $_GET['minify'] ) )
+      {
+        $this->minify( $str );
+      }
+			{
+				$file = $this->opts['file']['write']['all'];
+			}
 
 			$response = file_put_contents( $file, $str );
 
@@ -405,7 +478,9 @@ class CSS_Processor
 			$str .= '</main>' . PHP_EOL;
 			$str .= '<footer>' . PHP_EOL;
 			$str .= '<div class="text-center"><small>';
-			$str .= 'Note: This page has been <a href="https://github.com/earth3300/ec01-css">automatically generated</a>. No header, footer, menus or sidebars are available.';
+			$str .= 'Note: This page has been <a href="';
+			$str .= $this->opts['link'];
+			$str .= '">automatically generated</a>. No header, footer, menus or sidebars are available.';
 			$str .= '</small></div>' . PHP_EOL;
 			$str .= '</footer>' . PHP_EOL;
 			$str .= '</html>' . PHP_EOL;
@@ -417,12 +492,150 @@ class CSS_Processor
 			return false;
 		}
 	}
-}
+} // End Class
+
+/**
+ * Minify CSS
+ *
+ * Minify CSS, but keep enough line breaks intact, so it is still readable.
+ */
+class MinifyCSS extends CSSProcessor
+{
+	/**
+	* CSS Minifier => http://ideone.com/Q5USEF + improvement(s)
+	*/
+	protected function minify($input)
+	{
+		if(trim($input) === "")
+		{
+			return $input;
+		}
+
+		return preg_replace(
+			array(
+				// Remove comments
+				'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')|\/\*(?!\!)(?>.*?\*\/)#s',
+				// Remove unused white-spaces
+				'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/))|\s*+;\s*+(})\s*+|\s*+([*$~^|]?+=|[{};,>~+]|\s*+-(?![0-9\.])|!important\b)\s*+|([[(:])\s++|\s++([])])|\s++(:)\s*+(?!(?>[^{}"\']++|"(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')*+{)|^\s++|\s++\z|(\s)\s+#si',
+				// Replace `0(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)` with `0`
+				'#(?<=[:\s])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)#si',
+				// Replace `:0 0 0 0` with `:0`
+				'#:(0\s+0|0\s+0\s+0\s+0)(?=[;\}]|\!important)#i',
+				// Replace `background-position:0` with `background-position:0 0`
+				'#(background-position):0(?=[;\}])#si',
+				// Replace `0.6` with `.6`, but only when preceded by `:`, `-`, `,` or a white-space
+				'#(?<=[:\-,\s])0+\.(\d+)#s',
+				// Minify string value
+				'#(\/\*(?>.*?\*\/))|(?<!content\:)([\'"])([a-z_][a-z0-9\-_]*?)\2(?=[\s\{\}\];,])#si',
+				'#(\/\*(?>.*?\*\/))|(\burl\()([\'"])([^\s]+?)\3(\))#si',
+				// Minify HEX color code
+				'#(?<=[:\-,\s]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3#i',
+				// Remove empty selectors
+				'#(\/\*(?>.*?\*\/))|(^|[\{\}])(?:[^\s\{\}]+)\{\}#s'
+			),
+			array(
+				'$1',
+				'$1$2$3$4$5$6$7',
+				'$1',
+				':0',
+				'$1:0 0',
+				'.$1',
+				'$1$3',
+				'$1$2$4$5',
+				'$1$2$3',
+				'$1$2'
+			),
+		trim($input));
+	}
+} // end class
+
+
+/*
+ * JS and CSS Minifier Class
+ * version: 1.0 (2013-08-26)
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Toni Almeida wrote this plugin, which proclaims:
+ * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK."
+ *
+ * This plugin uses online webservices from javascript-minifier.com and cssminifier.com
+ * This services are property of Andy Chilton, http://chilts.org/
+ *
+ * Copyrighted 2013 by Toni Almeida, promatik.
+ *
+ * Data Format:
+ * 
+ * $css = [ "css/main.css" => "css/main.min.css" ];
+ *
+ * $js = [ "js/main.js" => "js/main.min.js" ];
+*
+ *
+ * @link https://github.com/promatik/PHP-JS-CSS-Minifier/blob/master/minifier.php
+ *
+ * File: minifier.php
+ * Created: 2018-11-21
+ * Updated: 2018-11-21
+ * Time: 10:06 AM
+ */
+class Minifier extends CSSProcessor
+{
+  /**
+   * Minify CSS
+   *
+   * @param  [type] $arr [description]
+   * @return [type]      [description]
+   */
+	protected function minifyCSS($arr)
+  {
+		minify($arr, 'https://cssminifier.com/raw');
+	}
+
+  /**
+   * Minify JS
+   *
+   * @param  [type] $arr [description]
+   * @return [type]      [description]
+   */
+	protected function minifyJS( $arr )
+  {
+		minify($arr, 'https://javascript-minifier.com/raw');
+	}
+
+  /**
+   * Minify
+   *
+   * @param  [type] $arr [description]
+   * @param  [type] $url [description]
+   *
+   * @return [type]      [description]
+   */
+	protected function minify( $arr, $url )
+  {
+		foreach ( $arr as $key => $value )
+    {
+			$handler = fopen($value, 'w') or die("File <a href='" . $value . "'>" . $value . "</a> error!<br />");
+			fwrite($handler, getMinified($url, file_get_contents($key)));
+			fclose($handler);
+			echo "File <a href='" . $value . "'>" . $value . "</a> done!<br />";
+		}
+	}
+
+	protected function getMinified( $url, $content )
+  {
+		$postdata = array('http' => array(
+	        'method'  => 'POST',
+	        'header'  => 'Content-type: application/x-www-form-urlencoded',
+	        'content' => http_build_query( array('input' => $content) ) ) );
+		return file_get_contents($url, false, stream_context_create( $postdata ) );
+	}
+} // End Minifier Class
 
 /**
  * Callback from the ec01-css shortcode.
  *
- * Performs a check, instantiates the CSS_Processor class, then starts the process.
+ * Performs a check, instantiates the CSSProcessor class, then starts the process.
  *
  * @param array  $args['dir']
  *
@@ -432,7 +645,7 @@ function ec01_css( $args )
 {
 	if ( is_array( $args ) )
 	{
-		$ec01_css = new CSS_Processor();
+		$ec01_css = new CSSProcessor();
 		return $ec01_css -> get( $args );
 	}
 	else
@@ -472,6 +685,6 @@ else
 	 *
 	 * @return string
 	 */
-	$ec01_css = new CSS_Processor();
+	$ec01_css = new CSSProcessor();
 	echo $ec01_css->get();
 }
